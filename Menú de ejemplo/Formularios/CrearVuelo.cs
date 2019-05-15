0eac3f7f7e163c98b11ec8dbb0d0e2ef9ee9aeae
elias <eliasexequieldefilippi@gmail.com>
2019-05-15 00:40:46 -0300

    Registrar un VUelo

--- Menú de ejemplo/Formularios/CrearVuelo.cs
+++ Menú de ejemplo/Formularios/CrearVuelo.cs
@@ -48,10 +48,45 @@ namespace Menú_de_ejemplo.Formularios
 
         private void buttonConfirmarVuelo_Click(object sender, EventArgs e)
         {
-            //      [id_vuelo],[id_avion],[fecha_salida_vuelo],[id_asiento],[id_tipo_documento_pasajero],[numero_documento_pasajero],[id_tarifa_vuelo]
+           
             int id_vuelo = int.Parse(textBoxNumeroVuelo.Text);
-            DateTime fecha_salida_vuelo = dateTimeFechaVuelo.Value;
+
+
+            //DateTime fecha_salida_vuelo = dateTimeFechaVuelo.Value.Date;
+            DateTime fecha_salida_vuelo = Convert.ToDateTime(dateTimeFechaVuelo.Value.Date.ToString("yyyy-MM-dd"));
+            //string formatted = fecha_salida_vuelo.ToString("dd/M/yyyy");
             int id_avion = int.Parse(comboBoxAvionVuelo.SelectedValue.ToString());
+            int id_tarifa_vuelo = int.Parse(comboBoxTarifaVuelo.SelectedValue.ToString());
+
+            insertarVuelo(id_vuelo, fecha_salida_vuelo, id_avion, id_tarifa_vuelo);
+           
+
+        }
+
+        private void insertarVuelo(int id_vuelo,DateTime fecha_salida_vuelo, int id_avion,int id_tarifa_vuelo)
+        {
+            string yourFormat = fecha_salida_vuelo.ToString("yyyyMMdd");
+
+            try
+            {
+
+                string insertVuelo = string.Format("INSERT INTO [LAFAST_gestor_de_reservas].[dbo].[vuelos] (nro_vuelo ,id_avion, fecha_salida_vuelo , id_tarifa_vuelo) VALUES (  {0} ,{1} , '{2}' , {3} );", id_vuelo, id_avion , fecha_salida_vuelo.ToString("yyyy-MM-dd"), id_tarifa_vuelo);
+
+                
+
+                MessageBox.Show("Se registro el vuelo número: " + id_vuelo);
+
+                this.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al cargar vuelo.- "+ e.Message );
+            }
+
+
+
         }
     }
 }
